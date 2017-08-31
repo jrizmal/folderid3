d21dec8918553c54d4d8845ef277396649309a1e
Jaka Rizmal <jrizmal@gmail.com>
2017-08-31 12:04:38 -0700

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  name = 'id3-folder-editor',
+  packages = ['id3-folder-editor'],
+  version = '0.1',
+  description = 'A tool for quick editing od id3 tags of audio files in a folder.',
+  author = 'Jaka Rizmal',
+  author_email = 'jrizmal@gmail.com',
+  url = 'https://github.com/peterldowns/mypackage',
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',
+  keywords = ['id3', 'music', 'mp3', 'tag', 'editor', 'id3 tag'],
+  classifiers = [],
+)
