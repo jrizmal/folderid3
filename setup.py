53ecd403bf9d9002f716658a1252cf96a75bf868
Jaka Rizmal <jrizmal@gmail.com>
2017-08-31 12:11:43 -0700

    Added the github url in setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
   description = 'A tool for quick editing od id3 tags of audio files in a folder.',
   author = 'Jaka Rizmal',
   author_email = 'jrizmal@gmail.com',
-  url = 'https://github.com/peterldowns/mypackage',
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',
+  url = 'https://github.com/jrizmal/folderid3',
+  #download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',
   keywords = ['id3', 'music', 'mp3', 'tag', 'editor', 'id3 tag'],
   classifiers = [],
 )
